ae31c3ec3dfe02fc2312403b3b17ddc8503573c0
yushanwebdev <yushanwebdev@gmail.com>
2022-12-03 19:15:50 +0530

    feat: Setup - Setup the Wesbos ESLint Config - Update the Pages file extensions to the `.tsx` from the `.js`

--- frontend/pages/account.tsx
+++ frontend/pages/account.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <p>Hello!</p>
+      <p>Account</p>
     </div>
   );
 }
--- frontend/pages/sell.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-      <div>
-        <p>Sell</p>
-      </div>
-    )
-  }
-  
