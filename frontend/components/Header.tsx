cea23cf191a9feff6d20642590108fd02a945986
yushanwebdev <yushanwebdev@gmail.com>
2022-12-04 16:59:24 +0530

    feat: CSS and Styled Components - Develop the stlyes of the Logo in the `<Header />` Component

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,11 +1,29 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
 
 export default function Header() {
   return (
     <div>
       <div className="bar">
-        <Link href="/">Sick Fits</Link>
+        <Logo>
+          <Link href="/">Sick Fits</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
