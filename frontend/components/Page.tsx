ed6bfd267d72b96cd5e4f9cb615086ef68e05929
yushanwebdev <yushanwebdev@gmail.com>
2022-12-04 16:14:43 +0530

    feat: Learning Next.js - Create the `<Header />` Component - Create the `<Nav />` Component

--- frontend/components/Page.tsx
+++ frontend/components/Page.tsx
@@ -1,6 +1,9 @@
+import Header from './Header';
+
 export default function Page({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      <Header />
       <h1>This the Page</h1>
       {children}
     </div>
