d5165f24e07d9f008ba68c213ece4593ea8842f4
yushanwebdev <yushanwebdev@gmail.com>
2022-12-04 19:22:29 +0530

    feat: CSS and Styled Components - Configure the NProgress in the `<App />`

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,16 @@
 import type { AppProps } from 'next/app';
+import { Router } from 'next/router';
+import NProgress from 'nprogress';
 import Page from '../components/Page';
+
+import 'nprogress/nprogress.css';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+Router.events.on('routeChangeStart', () => NProgress.start());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+Router.events.on('routeChangeComplete', () => NProgress.done());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
