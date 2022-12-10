b5e0fba7c75a9884caf8bd30533194a0219ca7b3
yushanwebdev <yushanwebdev@gmail.com>
2022-12-10 13:27:32 +0530

    backend: User Scehema - Develop & configure the User Schema in the Keystone

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -21,6 +22,7 @@ export default config({
   },
   lists: createSchema({
     // Schema items go in here
+    User,
   }),
   ui: {
     // Show the UI only for people who pass this test
