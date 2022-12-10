c4220f9204b17f1db0d2854ad16bd22e638793c0
yushanwebdev <yushanwebdev@gmail.com>
2022-12-10 21:05:52 +0530

    backend: Inserting Seed Data - Configure the `insertSeedData()` to execute after connect the Keystone to the DB

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import {
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -37,6 +38,11 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
+      async onConnect({ keystone }) {
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // Schema items go in here
