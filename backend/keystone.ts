38494483cb6359d661aa6746c7cb547fda09b048
yushanwebdev <yushanwebdev@gmail.com>
2022-12-10 12:56:18 +0530

    backend: Keystone Configurations - Configure the MongoDB & Session configurations in the Keystone

---
+++ backend/keystone.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long should the user stay signed in?
+  secret: process.env.COOKIES_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+  },
+  lists: createSchema({
+    // Schema items go in here
+  }),
+  ui: {
+    // Show the UI only for people who pass this test
+    isAccessAllowed: () => true,
+  },
+  // Add session values here
+});
