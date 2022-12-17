220990337be50850d6f02c07c2628ed4adf4b24b
yushanwebdev <yushanwebdev@gmail.com>
2022-12-17 13:33:48 +0530

    frontend: Fetching Data with hook & Displaying it in our Front End - Style the `<Product />` Component

--- frontend/components/Product.tsx
+++ frontend/components/Product.tsx
@@ -1,3 +1,22 @@
+import Link from 'next/link';
+import formatMoney from '../lib/formatMoney';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import TitleStyles from './styles/TitleStyles';
+
 export default function Product({ product }) {
-  return <p>{product.name}</p>;
+  return (
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product.name}
+      />
+      <TitleStyles>
+        <Link href={`/product/${product.id}`}>{product.name}</Link>
+      </TitleStyles>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product.description}</p>
+      {/* TODO: Add buttons to edit and delete item */}
+    </ItemStyles>
+  );
 }
