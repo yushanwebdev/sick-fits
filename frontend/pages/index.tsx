d9fcabe70fdef7e7b169c1c5293a9f0f0cb5e271
yushanwebdev <yushanwebdev@gmail.com>
2022-12-17 16:23:01 +0530

    frontend: Home Page Update - Configure the `<Home />` Page as a SSR Page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,3 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { ALL_PRODUCTS_QUERY } from '../components/Products';
+import { addApolloState, initializeApollo } from '../lib/apolloClient';
 import ProductsPage from './products';
+
+export async function getServerSideProps() {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: ALL_PRODUCTS_QUERY,
+  });
+
+  return addApolloState(apolloClient, {
+    props: {},
+  });
+}
 
 export default ProductsPage;
