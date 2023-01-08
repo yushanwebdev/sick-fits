72d0cf4f26d5bbbea7568503998d32e3147e0cbe
yushanwebdev <yushanwebdev@gmail.com>
2023-01-08 09:57:06 +0530

    frontend: User Registration + Authentication - Password Reset - Requesting a Reset

---
+++ frontend/components/RequestReset.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { gql, useMutation } from '@apollo/client';
+import useForm from '../lib/useForm';
+import Form from './styles/Form';
+import DisplayError from './DisplayError';
+
+const REQUEST_RESET_MUTATION = gql`
+  mutation SendPasswordResetRequest($email: String!) {
+    sendUserPasswordResetLink(email: $email) {
+      code
+      message
+    }
+  }
+`;
+
+export default function RequestReset() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+  });
+  const [signup, { data, error }] = useMutation(REQUEST_RESET_MUTATION, {
+    variables: inputs,
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // stop the form from submitting
+    // Send the email and password to the graphqlAPI
+    await signup().catch(console.error);
+    resetForm();
+  };
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Request a Password Reset</h2>
+      <DisplayError error={error} />
+
+      {data?.sendUserPasswordResetLink === null && (
+        <p>Success! Check your email for a link!</p>
+      )}
+
+      <fieldset>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In!</button>
+      </fieldset>
+    </Form>
+  );
+}
